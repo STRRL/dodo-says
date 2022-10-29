40b6ca5d8461618de731ac9dbffb9f2d68c811ec
STRRL <im@strrl.dev>
2022-10-29 19:20:49 +0800

    chore: default router
    Signed-off-by: STRRL <im@strrl.dev>

--- pkg/serverless/serverless.go
+++ pkg/serverless/serverless.go
@@ -4,7 +4,10 @@ import "github.com/gin-gonic/gin"
 
 func NewServerlessGinApp() *gin.Engine {
 	app := gin.Default()
-	router := app.Group("/api")
+	router := app.Group("/api/dodo")
+	router.GET("/", func(c *gin.Context) {
+		c.Data(200, "text/plain; charset=utf-8", []byte("🦤 dodo says hello."))
+	})
 	router.GET("/healthz", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "ok",
