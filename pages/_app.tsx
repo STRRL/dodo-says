c24caa4cbe0a48150dd1279e6e94400a51249c07
STRRL <im@strrl.dev>
2022-10-30 22:51:06 +0800

    feat: thread prototype
    Signed-off-by: STRRL <im@strrl.dev>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,30 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+
+import {
+  createDOMRenderer,
+  FluentProvider,
+  GriffelRenderer,
+  SSRProvider,
+  RendererProvider,
+  webLightTheme,
+} from '@fluentui/react-components';
+import type { AppProps } from 'next/app';
+import '../styles/globals.css';
 import { SessionProvider } from "next-auth/react"
 
-export default function App({
-  Component,
-  pageProps: { session, ...pageProps },
-}: AppProps) {
+type EnhancedAppProps = AppProps & { renderer?: GriffelRenderer };
+
+export default function App(
+  { Component, pageProps: { session, ...pageProps }, renderer }: EnhancedAppProps
+) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+      <RendererProvider renderer={renderer || createDOMRenderer()}>
+        <SSRProvider>
+          <FluentProvider theme={webLightTheme}>
+            <Component {...pageProps} />
+          </FluentProvider>
+        </SSRProvider>
+      </RendererProvider>
+    </SessionProvider >
   )
 }
