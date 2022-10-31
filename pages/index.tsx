79dd5f63856fc96554cfd5d786d371c5d185bfff
STRRL <im@strrl.dev>
2022-10-31 23:00:31 +0800

    feat(prototype): update index
    Signed-off-by: STRRL <im@strrl.dev>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,24 @@
 import { Display } from "@fluentui/react-components";
 import Link from "next/link";
+import useIdentity from "store/identity";
 
 export default function Home() {
+
+  const identity = useIdentity(state => state.identity);
+
+  if (identity) {
+    return (
+      <main className="m-20">
+        <Display block>Welcome, {identity.name}.</Display>
+        <Display block>Let&apos;s see <Link href={'/roost'} className="underline">roosts</Link>  you lived.</Display>
+      </main>
+    )
+  }
   return (
     <main className="m-20">
-      <Display>Welcome. Let&apos;s see <Link href={'/roost'}>roosts you lived.</Link></Display>
+      <Display block>Welcome Guest.</Display>
+      <Display block>Please Login to continue.</Display>
     </main>
   )
+
 }
